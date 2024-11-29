219c8237f396f4021bf4905bdda76dc3a688f429
margarida033 <168215543+margarida033@users.noreply.github.com>
2024-11-29 13:09:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    '1. Quando o Brasil se tornou uma república?',
+    'O Brasil se tornou uma república em 15 de novembro de 1889. Este evento marcou o fim do Império e a transição para um sistema republicano de governo'
+)
+
+criaCartao(
+    'Geografia',
+    'O Brasil se tornou uma república em 15 de novembro de 1889. Este evento marcou o fim do Império e a transição para um sistema republicano de governo',
+    'Tomé de Souza foi o primeiro governador-geral do Brasil, nomeado em 1548. Ele é conhecido por fundar a cidade de Salvador, que foi a primeira capital do Brasil'
+)
+
+criaCartao(
+    'Programação',
+    ' Qual foi a data da assinatura da Lei Áurea, que aboliu a escravidão no Brasil?',
+    'A Lei Áurea foi assinada em 13 de maio de 1888 pela Princesa Isabel, extinguindo oficialmente a escravidão no país'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'O que foi a Revolução de 1930 e qual seu impacto na política brasileira?',
+    'A Revolução de 1930 foi um movimento que resultou na deposição do presidente Washington Luís e na ascensão de Getúlio Vargas ao poder. Esse evento é considerado um divisor de águas na história do Brasil, levando a profundas mudanças sociais e políticas, incluindo a modernização da economia e a ampliação dos direitos sociais'
+)
